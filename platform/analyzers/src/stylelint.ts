4d83c0603970f3b1b09b8b48b86d73d31e3e0790
Jan Biasi <biasijan@gmail.com>
2019-04-01 16:08:11 +0200

    feat(analyzers): add stylelint analyzer

---
+++ platform/analyzers/src/stylelint.ts
@@ -0,0 +1,27 @@
+import { DependencyInstallation, fileExists, getDependencyInstallation } from '@namics/frontend-defaults-platform-core';
+import { join } from 'path';
+
+export type StylelintAnalyzerResult = {
+    stylelint: boolean;
+    stylelintIgnore?: boolean;
+    stylelintInstallation?: DependencyInstallation
+};
+
+export const stylelintAnalyzer = async (cwd: string): Promise<StylelintAnalyzerResult> => {
+    const doesStyleLintExist = await fileExists(join(cwd, 'stylelint.config.js'));
+
+    if (doesStyleLintExist) {
+        const doesStyleLintIgnoreExist = await fileExists(join(cwd, '.stylelintignore'));
+        const stylelintInstallation = await getDependencyInstallation(cwd, 'stylelint');
+
+        return {
+            stylelint: doesStyleLintExist,
+            stylelintIgnore: doesStyleLintIgnoreExist,
+            stylelintInstallation
+        };
+    }
+
+    return {
+        stylelint: doesStyleLintExist,
+    };
+}
---
+++ platform/analyzers/test/stylelint.test.ts
@@ -0,0 +1,41 @@
+import { getFixtureContext } from './utils';
+import { stylelintAnalyzer } from '../src/stylelint';
+
+const FIXTURE_INSTALLED = getFixtureContext('stylelint-project');
+const FIXTURE_NOT_INSTALLED = getFixtureContext('stylelint-project-not-installed');
+
+describe('Analyzers', () => {
+    describe('StylelintAnalyzer', () => {
+        it('should not crash', async () => {
+            expect(async () => await stylelintAnalyzer(FIXTURE_INSTALLED)).not.toThrow();
+            expect(async () => await stylelintAnalyzer(FIXTURE_NOT_INSTALLED)).not.toThrow();
+        });
+
+        it('should analyze a project with installation correctly', async () => {
+            const analytics = await stylelintAnalyzer(FIXTURE_INSTALLED);
+
+            expect(analytics.stylelint).toEqual(true);
+            expect(analytics.stylelintIgnore).toEqual(true);
+            expect(analytics.stylelintInstallation!.declared).toEqual('9.0.0');
+            expect(analytics.stylelintInstallation!.installed).toEqual('9.10.1');
+            expect(analytics).toMatchSnapshot({
+                stylelintInstallation: {
+                    latest: expect.any(String)
+                }
+            });
+        });
+
+        it('should analyze a project without ignore & installation correctly', async () => {
+            const analytics = await stylelintAnalyzer(FIXTURE_NOT_INSTALLED);
+
+            expect(analytics.stylelint).toEqual(true);
+            expect(analytics.stylelintIgnore).toEqual(false);
+            expect(analytics.stylelintInstallation!.installed).toBeFalsy();
+            expect(analytics).toMatchSnapshot({
+                stylelintInstallation: {
+                    latest: expect.any(String)
+                }
+            });
+        });
+    });
+});
